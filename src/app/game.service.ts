bafcf1bdc47b6a90d10aef89c6d86095fc6d7550
rpokosov <hasan83842@gmail.com>
2020-03-20 16:58:11 +0300

    intersects line

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -122,4 +122,16 @@ export class GameService {
       return false
     }
   }
+  
+  intersects(a,b,c,d,p,q,r,s) {
+    let det, gamma, lambda;
+    det = (c - a) * (s - q) - (r - p) * (d - b);
+    if (det === 0) {
+      return false;
+    } else {
+      lambda = ((s - q) * (r - a) + (p - r) * (s - b)) / det;
+      gamma = ((b - d) * (r - a) + (c - a) * (s - b)) / det;
+      return (0 < lambda && lambda < 1) && (0 < gamma && gamma < 1);
+    }
+  }
 }
