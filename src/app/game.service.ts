2366aef4bda2064bc604577e73d0efa8ec3943c1
rpokosov <hasan83842@gmail.com>
2020-03-23 16:31:24 +0300

    make check error

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -44,6 +44,8 @@ export class GameService {
   newGame(){
     this.generateItems()
     this.generateLines()
+    this.resetIntersectsLines();
+    this.takeLine()
     console.log(this.lines);
   }
 
@@ -55,6 +57,9 @@ export class GameService {
       this.replaceTile();
       this.resetChecked();
       this.generateLines();
+    this.resetIntersectsLines();
+      this.takeLine()
+    console.log(this.lines);
     }
   }
 
@@ -93,27 +98,77 @@ export class GameService {
     }
   }
 
- generateLines(){
-   this.lines = [];
-   for (let i = 1; i <= this.itemsLength; i++) {
-     let arr = this.items.filter(el=>el.ind===i);
-     let coordArr = [];
-     arr.forEach(el=>{
-       let obj = {top: el.top, left: el.left, id: i, intersects: null}
-       coordArr.push(obj)
-     });
-     this.lines.push(coordArr);
-   }
-   this.observableLines.next(this.lines);
- }
+  generateLines(){
+    this.lines = [];
+    for (let i = 1; i <= this.itemsLength; i++) {
+      let arr = this.items.filter(el=>el.ind===i);
+      let coordArr = [];
+      arr.forEach(el=>{
+        let obj = {top: el.top, left: el.left, id: i, intersects: null}
+        coordArr.push(obj)
+      });
+      this.lines.push(coordArr);
+    }
+    this.observableLines.next(this.lines);
+  }
 
- checkLines(){
-   this.lines.forEach(el=>{
-     el.forEach(el=>{
+  resetIntersectsLines(){
+    this.lines.forEach(ael=>{
+      ael.forEach(bel=>{
+        bel.intersects = false;
+      });
+    });
+  }
 
-     });
-   });
- }
+  takeLine(){
+    for(let i=0;i<=this.lines.length-1;i++){
+      let startLine = null,
+          endLine = null;
+      if( Array.isArray(this.lines[i]) ){
+        for(let j=0;j<=this.lines[i].length-1;j++){
+          let a = this.lines[i][j].top;
+          let b = this.lines[i][j].left;
+          let next = j === this.lines[i].length - 1 ? 0 : j+1;
+          let c = this.lines[i][next].top;
+          let d = this.lines[i][next].left;
+          
+          this.lines[i][j].intersects = this.checkIntersectsLine(a,b,c,d)
+        }
+      }
+    }
+  }
+  checkIntersectsLine(a,b,c,d){
+    for(let i=0;i<=this.lines.length-1;i++){
+      let startLine = null,
+          endLine = null,
+          intersects = null;
+
+          // console.log(this.lines[i]);
+      if( Array.isArray(this.lines[i]) ){
+        for(let j=0;j<=this.lines[i].length-1;j++){
+          // console.log(i, j);
+          let p = this.lines[i][j].top;
+          let q = this.lines[i][j].left;
+          let next = j === this.lines[i].length - 1 ? 0 : j+1;
+          // console.log(j, this.lines[i].length, next, this.lines[i][next]);
+          let r = this.lines[i][next].top;
+          let s = this.lines[i][next].left;
+
+          if( a!==p && b!==q && c!==r && d!==s){
+            intersects = this.intersects(a,b,c,d,p,q,r,s)
+          }else{
+            intersects = false
+          }
+          // console.log(intersects);
+          if(intersects === true){
+            return true;
+          }
+        }
+      }
+      
+    }
+    return false;
+  }
 
   randomFromMinToMax(min, max) {
     let rand = min + Math.random() * (max + 1 - min);
